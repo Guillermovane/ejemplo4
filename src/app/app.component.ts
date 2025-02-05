0da3951982e9db631aeddfaa25e7f0153086f0be
Guillermovane <guillermovb.ti22@utsjr.edu.mx>
2025-02-05 11:47:35 -0600

    nuevo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ejemplo4';
+
+  //propiedades
+  nombreCliente = "";
+  emailCliente = "";
+  opinionCliente= "";
+  statusCliente= "";
+  municipioCliente ="";
+  autorizacion= false;
+
+  //declaramos el formulario 
+  formOpinion = new FormGroup({
+    nombre: new FormControl(""),
+    email: new FormControl(""),
+    opinion: new FormControl(""),
+    status: new FormControl(""),
+    municipio: new FormControl(""),
+    autorizacion: new FormControl(false),
+  });
+
+  //método que se ejecuta cuando se envia el formulario
+  onSubmit(){
+    this.nombreCliente = this.formOpinion.value.nombre!;
+    this.emailCliente = this.formOpinion.value.email!;
+    this.opinionCliente = this.formOpinion.value.opinion!;
+    this.statusCliente = this.formOpinion.value.status!;
+    this.municipioCliente = this.formOpinion.value.municipio!;
+    this.autorizacion = this.formOpinion.value.autorizacion!;
+  }
 }
